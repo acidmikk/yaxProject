2f47effbee61c08408f4aa2815d905679eab3e9e
Sevastyan <sv.acid6@retrowave.ru>
2018-12-25 01:18:23 +0400

    примеры

--- maincode.py
+++ maincode.py
@@ -2,23 +2,30 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
 from mainw import MainWindow
 from calc import Calc
+ftom
 
 
 class Osnv(QMainWindow, MainWindow):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, parent=None):
+        super(Osnv, self).__init__(parent)
         self.setupUi(self)
         self.pushButton.clicked.connect(self.calc)
+        self.callcalc = Cal(self)
 
     def calc(self):
-        exc = Cal
-        exc.show()
+        self.callcalc.show()
+
+    def foto(self):
+        self.callfoto.show(self)
 
 
 class Cal(QMainWindow, Calc):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, parent=None):
+        super(Cal, self).__init__(parent)
         self.setupUic(self)
+
+
+class Fot(QMainWindow, )
 
 
 app = QApplication(sys.argv)
